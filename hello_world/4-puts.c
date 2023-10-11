c1828c8f539a801aad1511a8b0b8f5525976a272
= <giritekaaarsene@gmail.com>
2023-10-11 07:15:52 -0700

    adding 4-puts.c file

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,9 +1,9 @@
 #include<stdio.h>
-int main(void)
-{
 /*
 *Functiont to print a text
 */
-puts("Programming is like building a multilingual puzzle");
+int main(void)
+{
+puts("Programming is like building a multilingual puzzle\n");
 return (0);
 }
