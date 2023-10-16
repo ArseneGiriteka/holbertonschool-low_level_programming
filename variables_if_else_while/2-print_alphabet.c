94d09f5c28d71285ceed0fcc142d5b61d66dc64f
= <giritekaaarsene@gmail.com>
2023-10-16 07:25:49 -0700

    adding the 2-point-alphabet.c file

--- variables_if_else_while/2-print_alphabet.c
+++ variables_if_else_while/2-print_alphabet.c
@@ -1,18 +1,18 @@
 #include <stdio.h>
 
 /**
- * main : Entry - point
+ * main - Entry point
  *
- * Return : Always 0 (success)
+ * Return: Always 0 (success)
  */
 int main(void)
 {
 char ch = 'a';
-while(ch <= 'z')
+while (ch <= 'z')
 {
 putchar(ch);
 ch++;
 }
- putchar('\n');
+putchar('\n');
 return (0);
 }
