d371fc6c1a86b7e7a8fc30f399fb7a32adf05926
= <giritekaaarsene@gmail.com>
2023-10-17 01:02:38 -0700

    Adding 5-print_number.c file

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+  char ch = '0';
+  while (ch <= '9')
+    {
+      putchar(ch);
+      ch++;
+    }
+  putchar('\n');
+  return (0);
+}
