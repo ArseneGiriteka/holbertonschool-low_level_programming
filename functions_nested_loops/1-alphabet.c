ab0f2ec2a0c9913c5e54a43603045fdd80eae6f1
= <giritekaaarsene@gmail.com>
2023-10-17 05:44:52 -0700

    adding file

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * print_alphabet - print the alphabet in lowercase
+ *
+ * Return: Always void.
+ */
+void print_alphabet()
+{
+int i = 0;
+while ('a' + i <= 'z')
+{
+putchar('a'+i);
+i++;
+}
+putchar('\n');
+return;
+}
