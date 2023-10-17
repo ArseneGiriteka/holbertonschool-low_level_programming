f528b15cf85961a5f373e80f4b063cc64ce64888
= <giritekaaarsene@gmail.com>
2023-10-17 12:52:02 -0700

    adding files

---
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * print_alphabet_x10 - print the alphabet in lowercase 10 times
+ *
+ * Return: Always void.
+ */
+void print_alphabet_x10(void)
+{
+int i = 0, t = 0;
+for (t = 0; t < 10; t++)
+{
+  
+while ('a' + i <= 'z')
+{
+putchar('a' + i);
+i++;
+}
+ 
+putchar('\n');
+i = 0;
+}
+return;
+}
