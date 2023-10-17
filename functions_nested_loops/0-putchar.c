b961644d3dafa50f6b3e11d473d29025e60e1a92
= <giritekaaarsene@gmail.com>
2023-10-17 02:42:21 -0700

    adding

---
+++ functions_nested_loops/0-putchar.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+char word_string[] = "_putchar";
+int i = 0;
+while (word_string != '\0')
+{
+putchar(word_string[i]);
+i++;
+}
+putchar('\n');
+return (0);
+}
