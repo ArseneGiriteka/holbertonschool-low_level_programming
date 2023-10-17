c3e5b52c1e6a5aeeafd5de42e0c7e4de96f4a5aa
= <giritekaaarsene@gmail.com>
2023-10-17 13:30:28 -0700

    adding files

--- functions_nested_loops/3-islower.c
+++ functions_nested_loops/3-islower.c
@@ -3,10 +3,18 @@
 /**
  * _islower - Checks if a character given in parameter is lowercase
  *
+ * @ch: is a character passed in parameters
+ *
  * Return: returns 1 if character is in lowercase and 0 otherwise
  */
 int _islower(char ch)
 {
-  if (ch >= 'a' && ch <= 'z') return (1);
-  else return (0);
+if (ch >= 'a' && ch <= 'z')
+{
+return (1);
+}
+else
+{
+return (0);
+}
 }
