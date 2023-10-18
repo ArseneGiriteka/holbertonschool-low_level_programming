8b461dc66cf61adc732458bf3f46f54680d8ed90
= <giritekaaarsene@gmail.com>
2023-10-18 01:28:51 -0700

    it works

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/*
+ * _abs - compute absolute values of integers
+ *
+ * @n: integer value passed in parameter
+ *
+ * Return: absolute value of a number
+ */
+int _abs(int n)
+{
+	if (n < 0)
+	{
+		return (n * -1);
+	}
+	else
+	{
+		return (n);
+	}
+}
--- functions_nested_loops/main.h
+++ functions_nested_loops/main.h
@@ -1,6 +1,7 @@
-int _putchar(char ch);
+int _putchar(char);
 void print_alphabet(void);
 void print_alphabet_x10(void);
-int _islower(char ch);
-int _isalpha(char ch);
-int print_sign(int n);
+int _islower(char);
+int _isalpha(char);
+int print_sign(int);
+int _abs(int);
