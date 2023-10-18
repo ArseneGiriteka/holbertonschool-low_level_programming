ca97cc34656f0e175bec95573a6b2332509a469e
= <giritekaaarsene@gmail.com>
2023-10-18 00:24:46 -0700

    adding files

---
+++ functions_nested_loops/5-sign.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+/**
+ * print_sign - figures is the sign of an int
+ *
+ * @n: a number to check its sign
+ *
+ * Return: 1 if n > 0, -1 if n < 0 , 0 otherwise
+ */
+int print_sign(int n)
+{
+char ch = ' ';
+char return_value = 0;
+if (n > 0)
+{
+  putchar('+');
+  return (1);
+}
+else if (n == 0)
+{
+  putchar('0');
+  return (0)
+}
+else
+{
+  putchar('-');
+  return (-1);
+}
+}
