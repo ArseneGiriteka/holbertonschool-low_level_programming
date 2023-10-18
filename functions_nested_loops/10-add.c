bb22f34a8d31ca5ab5e34b5a56188867dd1c4f98
= <giritekaaarsene@gmail.com>
2023-10-18 06:33:19 -0700

    adding file

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * add - Add two int
+ *
+ * @a: first parameter
+ *
+ * @b: second parameter
+ *
+ * Return: Always return the addition of @a and @b
+ */
+int add(int a, int b)
+{
+	return (a+b);
+}
