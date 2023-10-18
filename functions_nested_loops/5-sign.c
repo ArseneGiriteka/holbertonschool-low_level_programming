f88132d89cae30a82a8569eb87289ed5499db64c
= <giritekaaarsene@gmail.com>
2023-10-18 01:15:56 -0700

    fixed!

--- functions_nested_loops/5-sign.c
+++ functions_nested_loops/5-sign.c
@@ -1,26 +1,20 @@
-#include <stdio.h>
-/**
- * print_sign - figures is the sign of an int
- *
- * @n: a number to check its sign
- *
- * Return: 1 if n > 0, -1 if n < 0 , 0 otherwise
- */
+#include "main.h"
+
 int print_sign(int n)
 {
-if (n > 0)
-{
-putchar('+');
-return (1);
-}
-else if (n == 0)
-{
-putchar('0');
-return (0);
-}
-else
-{
-putchar('-');
-return (-1);
-}
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n == 0)
+	{
+		_putchar('0');
+		return (0);
+	}
+	else
+	{
+		_putchar('-');
+		return (-1);
+	}
 }
