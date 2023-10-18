14f3400ba04f93ee80c0c8642e9af91cd7513206
= <giritekaaarsene@gmail.com>
2023-10-18 07:02:04 -0700

    fixed

---
+++ functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,51 @@
+#include "main.h"
+
+void print_i(int);
+
+/**
+ * print_to_98 - printo
+ *
+ * @n: parameter
+ *
+ *  Return: Always void
+ */
+void print_to_98(int n)
+{
+	int i = 0;
+
+	if (n > 98)
+	{
+		for (i = n; i >= 98; i--)
+		{
+			print_i(i);
+		}
+	}
+	else
+	{
+		for (i = n; i <= 98; i++)
+		{
+			print_i(i);
+		}
+
+	}
+	putchar('\n');
+}
+
+/**
+ * print_i - print a int
+ *
+ * @i: parameter
+ *
+ * Return: Always void
+ */
+void print_i(int i)
+{
+	if( i != 98)
+	{
+		printf("%d ,",i);
+	}
+	else
+	{
+		printf("%d\n",i);
+	}
+}
