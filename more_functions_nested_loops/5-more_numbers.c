ef9f92a2c23408fe13dd0eb0c2301526bec6c515
= <giritekaaarsene@gmail.com>
2023-10-20 07:00:56 -0700

    fixed

---
+++ more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * more_numbers - print numbers
+ *
+ * Return: nothing
+ */
+void more_numbers(void)
+{
+	int n = 0, i = 0;
+
+	while (n < 10)
+	{
+		for (i = 0; i <= 14, i++)
+		{
+			_putchar('0' + i);
+		}
+		_putchar('\n');
+		n++;
+	}
+}
