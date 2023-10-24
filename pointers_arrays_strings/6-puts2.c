dffb577d65e8e9fc502028715d8891a7788f096a
= <giritekaaarsene@gmail.com>
2023-10-24 04:32:58 -0700

    adding file

---
+++ pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * puts2 - print a string
+ *
+ * @str: string value
+ *
+ * Return: Always nothing
+ */
+void puts2(char *str)
+{
+	int i = 0;
+
+	while (*(str + i) != '\0')
+	{
+		_putchar(*(str + i));
+		i += 2;
+	}
+	_putchar('\n');
+}
