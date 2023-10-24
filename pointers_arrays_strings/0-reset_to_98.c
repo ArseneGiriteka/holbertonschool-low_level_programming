956b59697a6df0fd6ab2c5020de4d188dbdded28
= <giritekaaarsene@gmail.com>
2023-10-24 01:10:39 -0700

    adding files

---
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - function to set to 98
+ *
+ * @n: is a value that pointers to a int
+ *
+ * Return: Allways nothing
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
