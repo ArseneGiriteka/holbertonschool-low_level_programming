8b5d14f128e63b44632d0d42dead30bff7990646
= <giritekaaarsene@gmail.com>
2023-10-24 05:15:55 -0700

    adding stuffs

---
+++ pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * puts_half - print the scond half
+ *
+ * @str: string value
+ *
+ * Return: always nothing
+ */
+void puts_half(char *str)
+{
+	int length = 0, i = 0;
+
+	while (*(str + i))
+	{
+		length++;
+		i++;
+	}
+
+	for (i = ((length - (length % 2)) / 2); i < length; i++)
+	{
+		_putchar(*(str + i));
+	}
+	_putchar('\n');
+}
