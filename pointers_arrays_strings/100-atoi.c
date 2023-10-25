f9460705ef85e6f4d9ca74a927ef646e43b554f2
= <giritekaaarsene@gmail.com>
2023-10-25 04:48:29 -0700

    Gotta

---
+++ pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,42 @@
+#include "main.h"
+
+/**
+ * _atoi - prase string to int
+ *
+ * @s: a string
+ *
+ * Return: an integer
+ */
+int _atoi(char *s)
+{
+	int signe = 1;
+	int i = 0, result = 0;
+
+	while (*(s + i))
+	{
+		while (*(s + i) == '-')
+		{
+			signe *= -1; 
+			i++;
+			if (!((*(s + i) <= '9' && *(s + i) >= '0') || (*(s + i) == ' ') || (*(s + i) == '-') || (*(s + i) == '+')))
+			{
+				signe = 1;
+			}
+		}
+
+		while (*(s + i) <= '9' && *(s + i) >= '0')
+		{
+			result *= 10;
+			result += (*(s + i) - 48);
+			i++;
+		}
+
+		if (result != 0)
+		{
+			return (result * signe);
+		}
+
+		i++;
+	}
+	return (result);
+}
