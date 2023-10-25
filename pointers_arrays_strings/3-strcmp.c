69ff7e4d8904ee0c61a85dfb966b46ae6c8a27d3
= <giritekaaarsene@gmail.com>
2023-10-25 07:15:59 -0700

    gotta

--- pointers_arrays_strings/3-strcmp.c
+++ pointers_arrays_strings/3-strcmp.c
@@ -11,21 +11,11 @@
  */
 int _strcmp(char *s1, char *s2)
 {
-	int sum1 = 0, sum2 = 0, i = 0;
+	int i = 0;
 
-	while (*(s1 + i))
+	while (*(s1 + i) && (*(s1 + i) == *(s2 + i)))
 	{
-		sum1 += *(s1 + i);
 		i++;
 	}
-
-	i = 0;
-
-	while (*(s2 + i))
-	{
-		sum2 += *(s2 + i);
-		i++;
-	}
-
-	return (sum1 - sum2);
+	return (*(s1 + i) - *(s2 + i));
 }
