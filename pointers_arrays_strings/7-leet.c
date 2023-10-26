d36ef5ac625e25aada3872c2b9e3bd4d61e52ce4
= <giritekaaarsene@gmail.com>
2023-10-26 01:36:54 -0700

    adding

--- pointers_arrays_strings/7-leet.c
+++ pointers_arrays_strings/7-leet.c
@@ -11,20 +11,15 @@ char *leet(char *str)
 {
 	unsigned int i = 0;
 
-	int x = 0, length = 0;
+	int x = 0;
 
 	char ch[] = {'a', 'e', 'o', 't', 'l'};
 
 	char rep_ch[] = {'4', '3', '0', '7', '1'};
 
-	while (*(ch + length))
-	{
-		length++;
-	}
-
 	while (*(str + i))
 	{
-		for (x = 0; x < length; x++)
+		for (x = 0; x < 5; x++)
 		{
 			if ((*(str + i) == *(ch + x)) || (*(str + i) == (*(ch + x) - 'a' + 'A')))
 			{
