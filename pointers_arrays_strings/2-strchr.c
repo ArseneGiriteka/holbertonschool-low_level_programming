2f1419f4603abccdf7d5b67a00ab9de7cfc755d0
= <giritekaaarsene@gmail.com>
2023-10-26 06:34:04 -0700

    adding files

---
+++ pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * _strchr - function
+ *
+ * @s: a string value
+ *
+ * @c: a character value
+ *
+ * Return: a pointer to a char value
+ */
+char *_strchr(char *s, char c)
+{
+	unsigned int i = 0;
+
+	while (s + (i * sizeof(char)))
+	{
+		if (*(s + i) == c)
+		{
+			return (s + (i * sizeof(char)));
+		}
+		i++;
+	}
+	return (0);
+}
