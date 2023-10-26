f0aa60258d965de748b724cd01c6c9291565050c
= <giritekaaarsene@gmail.com>
2023-10-26 05:51:22 -0700

    adding file

--- pointers_arrays_strings/1-memcpy.c
+++ pointers_arrays_strings/1-memcpy.c
@@ -15,22 +15,14 @@ char *_memcpy(char *dest, char *src, unsigned int n)
 {
 	unsigned int i = 0, src_length = 0;
 
-	while (*(src + src_length))
+	while (*(src + (src_length * sizeof(char))))
 	{
 		src_length++;
 	}
 
 	while ((src + (i * sizeof(char))) && (i < n))
 	{
-		*(dest + i) = *(src + i);
-		i++;
-	}
-
-	i = 0;
-
-	while (src_length + i <= n)
-	{
-		*(dest + i) = '\0';
+		(*(dest + i)) = (*(src + i));
 		i++;
 	}
 	return (dest);
