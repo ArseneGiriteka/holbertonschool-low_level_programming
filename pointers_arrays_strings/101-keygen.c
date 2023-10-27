2b109b5344571c180f97886ec85f9ed769628552
= <giritekaaarsene@gmail.com>
2023-10-27 04:19:47 -0700

    adding

---
+++ pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+char generate_random_char();
+
+/**
+ * main - to check the code
+ *
+ * Return: an int value
+ */
+int main(void)
+{
+	char *key;
+	int i = 0, length = 0;
+	time_t t;
+	
+	srand((unsigned) (time(&t)));
+	length = (rand() % 50);
+	while (i < length)
+	{
+		key[i] = generate_random_char();
+		i++;
+	}
+
+	printf("%s", key);
+
+	return (0);
+}
+
+/**
+ * generate_random_char - function
+ *
+ * Return: a char value
+ */
+char generate_random_char()
+{
+	char value;
+	value = 41 + (rand() % (127 - 41));
+	return value;
+}
