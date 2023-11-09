7d0c1510039de75ee5f7e3a57122a8b5484701b6
= <giritekaaarsene@gmail.com>
2023-11-09 01:06:38 -0800

    adding files

--- malloc_free/0-create_array.c
+++ malloc_free/0-create_array.c
@@ -14,7 +14,10 @@ char *create_array(unsigned int size, char c)
 	unsigned int i = 0;
 	char *array;
 
-	array = malloc(sizeof(char) * (size + 1));
+	if (size == 0)
+		return (NULL);
+
+	array = malloc(sizeof(char) * size);
 
 	if (array == NULL)
 		return (NULL);
@@ -24,7 +27,5 @@ char *create_array(unsigned int size, char c)
 		*(array + i) = c;
 		i++;
 	}
-	*(array + i) = '\0';
-
 	return (array);
 }
