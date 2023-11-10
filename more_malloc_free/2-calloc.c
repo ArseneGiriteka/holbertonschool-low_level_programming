d63ec7bcb91a17547e84798aed45586a705dc565
= <giritekaaarsene@gmail.com>
2023-11-09 17:24:18 -0800

    adding files

---
+++ more_malloc_free/2-calloc.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * _calloc - function
+ * @nmemb: integer value
+ * @size: integer
+ * Return: a pointer
+ */
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	unsigned int i = 0;
+	char *array;
+
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+
+	array = malloc(nmemb * size);
+	if (array == NULL)
+		return (NULL);
+
+	for (i = 0; i < (size * nmemb); i++)
+		*(array + i) = 0;
+
+	return (array);
+}
