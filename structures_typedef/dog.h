bc19a17dd65b5c8309690bd301af724da82f70f9
= <giritekaaarsene@gmail.com>
2023-11-14 00:53:16 -0800

    adding file

---
+++ structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef HEADER_FILE_H
+#define HEADER_FILE_H
+
+/**
+ * struct dog - a dog structure
+ * @age: an age of the dog
+ * @name: a string that gives the name of the dog
+ * @owner: a string that gives the name of the dog owner
+ */
+struct dog
+{
+	char *name;
+	double age;
+	char *owner;
+};
+
+#endif
