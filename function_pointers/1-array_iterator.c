2694be3a028d1fd6b414499998f9af7d62e01998
= <giritekaaarsene@gmail.com>
2023-11-15 11:06:40 -0800

    adding files

---
+++ function_pointers/1-array_iterator.c
@@ -0,0 +1,21 @@
+#include "function_pointers.h"
+
+/**
+ * array_iterator - function
+ * @array: array of int
+ * @size: the size of @array
+ * @action: function to process @array elements
+ * Return: nothing
+ */
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	unsigned int i = 0;
+	if (action != NULL)
+	{
+		while (i < size)
+		{
+			action(*(array + i));
+			i++;
+		}
+	}
+}
