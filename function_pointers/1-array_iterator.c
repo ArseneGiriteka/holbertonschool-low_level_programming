e2e4e3b9c608d73710d9a2422f0f29b29bddc808
= <giritekaaarsene@gmail.com>
2023-11-15 11:21:49 -0800

    adding files

--- function_pointers/1-array_iterator.c
+++ function_pointers/1-array_iterator.c
@@ -12,7 +12,7 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 	unsigned int i = 0;
 	if (action != NULL && array != NULL)
 	{
-		while (i < size)
+		while (i < size || (array + i) != NULL)
 		{
 			action(*(array + i));
 			i++;
