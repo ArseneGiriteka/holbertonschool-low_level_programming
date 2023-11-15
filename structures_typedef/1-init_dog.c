25424e47cad4056de6fc197359009c38ad445333
= <giritekaaarsene@gmail.com>
2023-11-15 09:40:30 -0800

    fixed

--- structures_typedef/1-init_dog.c
+++ structures_typedef/1-init_dog.c
@@ -9,43 +9,7 @@
  */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-	unsigned int i = 0;
-
-	while (*(name + i))
-	{
-		i++;
-	}
-
-	d->name = malloc(sizeof(char) * (i + 1));
-	if (d->name == NULL)
-	{
-		return;
-	}
-	i = 0;
-	while (*(name + i))
-	{
-		*(d->name + i) = *(name + i);
-		i++;
-	}
-	*(d->name + i) = '\0';
-
-	i = 0;
-	while (*(owner + i))
-	{
-		i++;
-	}
-	d->owner = malloc(sizeof(char) * (i + 1));
-	if (d->owner == NULL)
-	{
-		return;
-	}
-	i = 0;
-	while (*(owner + i))
-	{
-		*(d->owner + i) = *(owner + i);
-		i++;
-	}
-	*(d->owner + i) = '\0';
-
+	d->name = name;
 	d->age = age;
+	d->owner = owner;
 }
