176b1128982a11bab7d44a05d1c600dc5f9fe221
= <giritekaaarsene@gmail.com>
2023-11-15 11:57:32 -0800

    adding file

--- structures_typedef/1-init_dog.c
+++ structures_typedef/1-init_dog.c
@@ -1,5 +1,6 @@
 #include "dog.h"
 
+unsigned int _length(char *);
 /**
  * init_dog - function that initialize a dog structure
  * @d: the dog structure
@@ -9,9 +10,55 @@
  */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-	if (owner == NULL || name == NULL || *name == '\0' || *owner == '\0')
-		exit(98);
-	d->name = name;
+	unsigned int i = 0;
+
+	if (name == NULL || owner == NULL)
+		exit(0);
+
+	d->name = malloc(sizeof(char) * (_length(name) + 1));
+	if (d->name == NULL)
+		exit (0);
+	
+	while (*(name + i))
+	{
+		*(d->name + i) = *(name + i);
+		i++;
+	}
+	*(d->name + i) = '\0';
+
+	d->owner = malloc(sizeof(char) * (_length(owner) + 1));
+	if (d->owner == NULL)
+	{
+		free(d->name);
+		exit(0);
+	}
+	i = 0;
+	while (*(owner + i))
+	{
+		*(d->owner + i) = *(owner + i);
+		i++;
+	}
+	*(d->owner + i) = '\0';
+
 	d->age = age;
-	d->owner = owner;
+}
+
+/**
+ * _length - function
+ * @str: a string
+ * Return: unsigned int value
+ */
+unsigned int _length(char *str)
+{
+	unsigned int i = 0;
+
+	if (str == NULL)
+		return (0);
+
+	while (*(str + i))
+	{
+		i++;
+	}
+
+	return (i);
 }
