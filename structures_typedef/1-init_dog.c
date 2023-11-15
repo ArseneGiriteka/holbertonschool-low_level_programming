dc52829d45a8439aab1c1251a415472e6c97b7d1
= <giritekaaarsene@gmail.com>
2023-11-15 09:44:42 -0800

    fixed

--- structures_typedef/1-init_dog.c
+++ structures_typedef/1-init_dog.c
@@ -9,6 +9,8 @@
  */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
+	if (owner == NULL || name == NULL || name == "" || owner == "")
+		exit(98);
 	d->name = name;
 	d->age = age;
 	d->owner = owner;
