489846fe59ab4b2721b59e847fa2bf7162e15cee
= <giritekaaarsene@gmail.com>
2023-11-15 16:51:11 -0800

    adding file

---
+++ function_pointers/2-int_index.c
@@ -0,0 +1,46 @@
+#include "function_pointers.h"
+
+int _length(int *);
+
+/**
+ * int_index - function
+ * @array: array of integer
+ * @size: size of @array
+ * @cmp: function pointer
+ * Return: integer
+ */
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i = 0;
+
+	if (size <= 0 || array == NULL)
+		return (-1);
+
+	while (i < size)
+	{
+		if (cmp(*(array + i)) != 0)
+			return (i);
+		i++;
+	}
+
+	return (-1);
+}
+
+/**
+ * _length - function
+ * @array: array of int
+ * Return: integer
+ */
+int _length(int *array)
+{
+	int i = 0;
+
+	if (array == NULL)
+		return (0);
+
+	while ((array + i) != NULL)
+	{
+		i++;
+	}
+	return (i);
+}
