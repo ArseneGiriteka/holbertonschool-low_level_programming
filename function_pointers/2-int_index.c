d26571279fdb00f19e94de609ecc46060fbed69b
= <giritekaaarsene@gmail.com>
2023-11-15 17:13:29 -0800

    tryna fixing

--- function_pointers/2-int_index.c
+++ function_pointers/2-int_index.c
@@ -1,13 +1,11 @@
 #include "function_pointers.h"
 
-int _length(int *);
-
 /**
  * int_index - function
  * @array: array of integer
  * @size: size of @array
  * @cmp: function pointer
- * Return: integer
+ * Return: an index
  */
 int int_index(int *array, int size, int (*cmp)(int))
 {
@@ -15,9 +13,6 @@ int int_index(int *array, int size, int (*cmp)(int))
 
 	if (size <= 0 || array == NULL)
 		return (-1);
-	
-	if (size < _length(array))
-		size = _length(array);
 
 	while (i < size)
 	{
@@ -27,23 +22,4 @@ int int_index(int *array, int size, int (*cmp)(int))
 	}
 
 	return (-1);
-}
-
-/**
- * _length - function
- * @array: array of int
- * Return: integer
- */
-int _length(int *array)
-{
-	int i = 0;
-
-	if (array == NULL)
-		return (0);
-
-	while ((array + i) != NULL)
-	{
-		i++;
-	}
-	return (i);
 }
