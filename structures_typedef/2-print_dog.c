25ece2106cb7bf75459456c3e2c19a97c0dd7c01
= <giritekaaarsene@gmail.com>
2023-11-17 03:21:15 -0800

    adding file

--- structures_typedef/2-print_dog.c
+++ structures_typedef/2-print_dog.c
@@ -12,7 +12,7 @@ void print_dog(struct dog *d)
 		if ((d->name) != NULL)
 			printf("Name: %s\n", d->name);
 		else
-			printf("(nil)\n");
+			printf("Name: (nil)\n");
 
 		printf("Age: %.6f\n", d->age);
 
@@ -21,6 +21,4 @@ void print_dog(struct dog *d)
 		else
 			printf("(nil)\n");
 	}
-	else
-		printf("\n");
 }
