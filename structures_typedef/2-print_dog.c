f90aab6cea6b9ef918d5f034d58cd6ecc3bf1437
= <giritekaaarsene@gmail.com>
2023-11-17 03:11:01 -0800

    adding file

--- structures_typedef/2-print_dog.c
+++ structures_typedef/2-print_dog.c
@@ -9,20 +9,20 @@ void print_dog(struct dog *d)
 {
 	if (d != NULL)
 	{
-		if (d->name != NULL)
+		if ((d->name) != NULL)
+		{
 			printf("Name: %s\n", d->name);
+		}
 		else
-			printf("Age: (nil)\n");
+		{
+			printf("(nil)\n");
+		}
 
 		printf("Age: %.6f\n", d->age);
 
 		if (d->owner != NULL)
 			printf("Owner: %s\n", d->owner);
 		else
-			printf("Owner: (nil)\n");
-	}
-	else
-	{
-		printf("\n");
+			printf("(nil)\n");
 	}
 }
