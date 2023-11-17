82fefc5226095ac4f895e1f1b04d214f44fee81c
= <giritekaaarsene@gmail.com>
2023-11-17 02:31:37 -0800

    adding file

--- structures_typedef/1-init_dog.c
+++ structures_typedef/1-init_dog.c
@@ -1,6 +1,6 @@
 #include "dog.h"
 
-unsigned int _length(char *);
+char *_copy(char *str);
 /**
  * init_dog - function that initialize a dog structure
  * @d: the dog structure
@@ -10,55 +10,39 @@ unsigned int _length(char *);
  */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-	unsigned int i = 0;
-
-	if (name == NULL || owner == NULL)
-		exit(1);
-
-	d->name = malloc(sizeof(char) * (_length(name) + 1));
-	if (d->name == NULL)
-		exit (0);
-	
-	while (*(name + i))
-	{
-		*(d->name + i) = *(name + i);
-		i++;
-	}
-	*(d->name + i) = '\0';
-
-	d->owner = malloc(sizeof(char) * (_length(owner) + 1));
-	if (d->owner == NULL)
-	{
-		free(d->name);
-		exit(0);
-	}
-	i = 0;
-	while (*(owner + i))
-	{
-		*(d->owner + i) = *(owner + i);
-		i++;
-	}
-	*(d->owner + i) = '\0';
-
+	d->name = _copy(name);
 	d->age = age;
+	d->owner = _copy(owner);
 }
 
 /**
- * _length - function
- * @str: a string
- * Return: unsigned int value
+ * _copy: function
+ * @str: string
+ * Return: a string
  */
-unsigned int _length(char *str)
+char *_copy(char *str)
 {
-	unsigned int i = 0;
+	unsigned int i = 0, len = 0;
+	char *copy;
 
 	if (str == NULL)
-		return (0);
+		return (NULL);
+
+	while (*(str + len))
+	{
+		len++;
+	}
+
+	copy = malloc(sizeof(char) * (len + 1));
+	if (copy == NULL)
+		return (NULL);
 
 	while (*(str + i))
 	{
+		*(copy + i) = *(str + i);
 		i++;
 	}
+	*(copy + i) = '\0';
 
-	return (i);
+	return (copy);
 }
