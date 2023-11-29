03130e7257a98da1a3a6b2a1ee45602d708086b5
= <giritekaaarsene@gmail.com>
2023-11-29 10:42:41 -0800

    fixing bug

--- singly_linked_lists/0-print_list.c
+++ singly_linked_lists/0-print_list.c
@@ -1,8 +1,6 @@
 #include "lists.h"
 
-char *_strdup(const char *);
 size_t show_list(const list_t *, const list_t *);
-size_t _depth(const list_t *);
 /**
  * print_list - print all elements of @h
  * @h: a list given in parameter
@@ -14,48 +12,20 @@ size_t print_list(const list_t *h)
 
 	if (h == NULL)
 		return (0);
-
+	size++;
 	if (h->str != NULL)
 		printf("[%d] %s\n", h->len, h->str);
 	else
 		printf("[0] (nil)\n");
 
 	if (h->next != NULL)
-		show_list(h->next, h);
+		size += show_list(h->next, h);
 
-	size = _depth(h);
 	printf("-> %lu elements", size);
-
 
 	return (size);
 }
 
-/**
- * _strdup - diplicates @str
- * @str: string value
- * Return: duplicate @string
- */
-char *_strdup(const char *str)
-{
-	char *copy;
-	int i = 0;
-
-	while (*(str + i))
-		i++;
-
-	copy = malloc(sizeof(char) * (i + 1));
-	if (copy == NULL)
-		return (copy);
-
-	*(copy + i) = '\0';
-	i--;
-	while (i >= 0)
-	{
-		*(copy + i) = *(str + i);
-		i--;
-	}
-	return (copy);
-}
 /**
  * show_list - function
  * @head: head node
@@ -73,29 +43,4 @@ size_t show_list(const list_t *list, const list_t *head)
 		printf("[0] (nil)\n");
 
 	return (1 + show_list(list->next, head));
-}
-/**
- * _depth - a function that count nodes of @list
- * @list: a list
- * Return: number of @list's nodes
- */
-size_t _depth(const list_t *list)
-{
-	size_t s = 1;
-	list_t *copy;
-
-	if (list->next == list)
-		return (1);
-
-	copy = list->next;
-	while (copy != NULL)
-	{
-		s++;
-		copy = copy->next;
-		if (copy == NULL || copy == list)
-			return (s);
-	}
-
-	return (s);
-
 }
