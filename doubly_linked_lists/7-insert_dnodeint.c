399ad70461eaee31ae29616e24379aac5c388b03
= <giritekaaarsene@gmail.com>
2023-11-30 15:26:56 -0800

    adding files

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -17,7 +17,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 		return (NULL);
 	node->n = n;
 
-	if (h == NULL && n == 0)
+	if (h == NULL && idx == 0)
 	{
 		free(node);
 		return (add_dnodeint(h, n));
