1622a8be4567feff34a793cce8ea937dd27c7d1c
= <giritekaaarsene@gmail.com>
2023-11-30 13:06:47 -0800

    fixing bug

--- doubly_linked_lists/2-add_dnodeint.c
+++ doubly_linked_lists/2-add_dnodeint.c
@@ -17,9 +17,16 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 	tmp->n = n;
 	tmp->prev = NULL;
 
-	while (*head != NULL && ((*head)->prev) != NULL)
+	if (head == NULL)
 	{
-		*head = ((*head)->prev);
+		tmp->next = NULL;
+		*head = tmp;
+		return (*head);
+	}
+
+	while (*head != NULL && (*head)->prev != NULL)
+	{
+		(*head) = (*head)->prev;
 	}
 
 	tmp->next = *head;
