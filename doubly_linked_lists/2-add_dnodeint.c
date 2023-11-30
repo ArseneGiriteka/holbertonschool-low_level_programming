762c7c81dfc1b95f3d75e39d909a14ebe239fb27
= <giritekaaarsene@gmail.com>
2023-11-30 11:03:36 -0800

    adding files

--- doubly_linked_lists/2-add_dnodeint.c
+++ doubly_linked_lists/2-add_dnodeint.c
@@ -13,14 +13,10 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 	tmp = malloc(sizeof(dlistint_t));
 	if (tmp == NULL)
 		return (NULL);
+
 	tmp->n = (int) n;
 	tmp->prev = NULL;
-
-	if (head != NULL)
-		tmp->next = *head;
-	else
-		tmp->next = NULL;
-
+	tmp->next = *head;
 	*head = tmp;
 
 	return (*head);
