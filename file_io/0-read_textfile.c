195671d982165d2341c4558c7396d980daee2d16
= <giritekaaarsene@gmail.com>
2023-12-07 03:10:49 -0800

    adding files

--- file_io/0-read_textfile.c
+++ file_io/0-read_textfile.c
@@ -1,6 +1,7 @@
 #include "main.h"
 
 ssize_t _length(char *str);
+void init(char *str, size_t size, char c);
 
 /**
  * read_textfile - function
@@ -26,24 +27,25 @@ ssize_t read_textfile(const char *filename, size_t letters)
 		close(file_d);
 		return (0);
 	}
-	*(buffer + letters) = '\0';
 	read_letters = read(file_d, buffer, letters);
+	*(buffer + letters) = '\0';
 	if (read_letters == -1)
 	{
 		free(buffer);
 		close(file_d);
 		return (0);
 	}
-	wrote_letters = write(STDOUT_FILENO, buffer, letters);
-	if (wrote_letters == -1 || (size_t) wrote_letters != letters)
+	wrote_letters = write(STDOUT_FILENO, buffer, (size_t) read_letters);
+	if (wrote_letters == -1 || wrote_letters != read_letters)
 	{
+		printf("YOOOOO");
 		free(buffer);
 		close(file_d);
 		return (0);
 	}
 	free(buffer);
 	close(file_d);
-	return (read_letters);
+	return (wrote_letters);
 }
 
 /**
@@ -62,4 +64,27 @@ ssize_t _length(char *str)
 		size++;
 
 	return (size);
+}
+
+/**
+ * init - funct
+ * @str: string value
+ * @size: size of str
+ * @c: char value
+ * Return: nothing
+ */
+void init(char *str, size_t size, char c)
+{
+	size_t i = 0;
+
+	if (str == NULL || size == 0)
+		return;
+
+	while (i < size)
+	{
+		*(str + i) = c;
+		i++;
+	}
+	*(str + i) = '\0';
+	return;
 }
