50ed7d54e2baec39ddac19dd12f414d754b0326f
= <giritekaaarsene@gmail.com>
2023-12-07 13:10:49 -0800

    adding files

---
+++ hash_tables/0-hash_table_create.c
@@ -0,0 +1,28 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_create - function
+ * @size: size of hash table to create
+ * Return: that hash table created
+ */
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	hash_table_t *table;
+
+	table = malloc(sizeof(hash_table_t));
+	if (table == NULL)
+		return (NULL);
+	table->size = size;
+	if (size == 0)
+		table->array = NULL;
+	else
+	{
+		table->array = malloc(sizeof(hash_node_t *) * size);
+		if (table->array == NULL)
+		{
+			free(table);
+			return (NULL);
+		}
+	}
+	return (table);
+}
