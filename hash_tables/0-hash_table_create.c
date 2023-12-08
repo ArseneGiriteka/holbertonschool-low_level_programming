f6776f3300455130f2486fc0f63d0b57920659f1
= <giritekaaarsene@gmail.com>
2023-12-08 04:12:53 -0800

    fixing 0

--- hash_tables/0-hash_table_create.c
+++ hash_tables/0-hash_table_create.c
@@ -21,7 +21,7 @@ hash_table_t *hash_table_create(unsigned long int size)
 		table->array = NULL;
 	else
 	{
-		table->array = malloc(sizeof(hash_node_t *) * size);
+		table->array = malloc(sizeof(hash_node_t *));
 		if (table->array == NULL)
 		{
 			free(table);
